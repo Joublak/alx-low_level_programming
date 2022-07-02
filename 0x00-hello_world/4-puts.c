010cb45a37ce5b5d1fe3376649de88885b66db5e
Unah <gloryunah7>
2022-07-02 09:22:09 -0700

    Modifying my scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
+#include <string.h>
+/**
  * main - Entry point
- * Return:Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzel");
-	return(0);
+	return (0);
 }
