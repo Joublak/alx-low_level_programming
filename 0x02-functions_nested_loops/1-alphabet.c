a158da788f959c0346fb4c4548c93eb0cf5747bd
Unah <gloryunah7>
2022-07-05 15:09:29 -0700

    fixing recent file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
  * main - prints alphabet in lower case
- * followed by a new line
- * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 	int j;
 
-	for (j = 'a'; j <= 'z'; j++
+	for (j = 'a'; j <= 'z'; j++);
 	{
-	_putchar(j);
+		_putchar('j');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
 
