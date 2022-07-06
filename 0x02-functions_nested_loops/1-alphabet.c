4ee43f5991e7de5bcd4a308bdc5a1c1fc3c2f106
Unah <gloryunah7>
2022-07-06 02:32:35 -0700

    Yet another fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (c = 'a'; c <= 'z'; c++);
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
