411bef297453f24e056cf6abc219593a0eaa9075
Unah <gloryunah7>
2022-07-06 19:40:00 -0700

    Adding 10x file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ * Return: 0
+ *    */
+void print_alphabet_x10(void)
+{
+	char r;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r);
+		}
+		_putchar('\n');
+	}
+}
