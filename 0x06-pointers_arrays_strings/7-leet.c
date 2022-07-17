9647faddfde0b6dc299b45c7272be97381422e5c
Unah <gloryunah7>
2022-07-17 08:31:36 -0700

    And more files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ char *leet(char *s)
 	int i, ii;
 	char s1[] = "aeotl";
 	char S1[] = "AEOTL";
-	char s2[] = "43071"
+	char s2[] = "43071";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++);
 	{
 		for (ii = 0; ii < 5; ii++)
 		{
