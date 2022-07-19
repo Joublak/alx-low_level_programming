1fba0105fae6dfc5d5324e18d3effef3d6d59205
Unah <gloryunah7>
2022-07-18 18:19:19 -0700

    Files 0 1 2 3 4 5 6 7

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,15 @@
  * @dest: destination
  * @src: source
  * @n: number of bytes to copy
- *
  * Return: a pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
+	unsigned int i;
 
-  for (i = 0; i < n; i++)
-    dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
-  return (dest);
+	return (dest);
 }
